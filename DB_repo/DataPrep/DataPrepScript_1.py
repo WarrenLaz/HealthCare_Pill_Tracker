9fb5fafd5a604070e134236dbdcd6a634a3441ff
Warren Lazarraga <79947453+WarrenLaz@users.noreply.github.com>
2024-09-30 18:13:25 -0400

    db scripts added

---
+++ DB_repo/DataPrep/DataPrepScript_1.py
@@ -0,0 +1,28 @@
+import requests
+from bs4 import BeautifulSoup as bfs
+
+# Read in HTML file
+Alpha = list("abcdefghijklmnopqrstuvwxyz")
+indexS = "https://www.drugs.com/alpha/"
+f = open('/Users/warren_lazarraga/Programming_projects/HealthCare_Pill_Tracker/DB_repo/DataPrep/DrugNames.txt', 'w')
+
+
+#start iteration aa ab ac ad...xz yz zz
+for alpha in Alpha:
+    for alph in Alpha:
+
+        # append "https://www.drugs.com/alpha/" to [xx] to query: pro=1
+        # Professional Monographs
+
+        linkpro = indexS + str(alpha) + str(alph) + ".html"
+        r2 = requests.get(linkpro)
+        soup2 = bfs(r2.content,'html5lib')
+
+        # Read all medication names
+        for med in soup2.find_all('ul', attrs={"class" : "ddc-list-column-2"}):
+            print(med.text)
+            f.write(med.text + "\n")
+print('done')
+f.close()
+        
+        
