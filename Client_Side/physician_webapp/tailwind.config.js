9ff54aa45de39854de9c441ba2b332d35440e1ef
Heba <hebasayed1603@gmail.com>
2024-11-01 20:38:28 -0400

    added tailwind and Inter font

---
+++ Client_Side/physician_webapp/tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#407BFF",
+      },
+      fontFamily: {
+        sans: [
+          "Inter",
+          "system-ui",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "Segoe UI",
+          "Roboto",
+          "Oxygen",
+          "Ubuntu",
+          "Cantarell",
+          "Fira Sans",
+          "Droid Sans",
+          "Helvetica Neue",
+          "sans-serif",
+        ],
+      },
+
+      components: {},
+    },
+  },
+  plugins: [],
+};
