ce556e8ef4afd0bf800e0afa7fee3b1bb69fab57
Heba Sayed <hebasayed1603@gmail.com>
2024-11-20 20:00:48 -0500

    form validation and styling for register

--- Client_Side/physician_webapp/src/components/Reg.js
+++ Client_Side/physician_webapp/src/components/Reg.js
@@ -1,50 +1,251 @@
 import React from "react";
-import { useState } from "react";
-import axios from 'axios'
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios from "axios";
+
+const schema = z
+  .object({
+    Practice_Email_Address: z
+      .string()
+      .email("Invalid email address")
+      .min(1, "Email is required"),
+    Password: z
+      .string()
+      .min(8, "Password must be at least 8 characters long")
+      .min(1, "Password is required"),
+    Confirm_Password: z
+      .string()
+      .min(8, "Confirm Password must be at least 8 characters long")
+      .min(1, "Confirm Password is required"),
+    First_Name: z.string().min(1, "First Name is required"),
+    Last_Name: z.string().min(1, "Last Name is required"),
+    Date_of_Birth: z.string().min(1, "Date of Birth is required"),
+    Practice_Phone_Number: z
+      .string()
+      .min(1, "Practice Phone Number is required")
+      .refine((val) => {
+        // Check if the phone number follows a simple US format
+        return /^\(?\d{3}\)?[-.\s]?\d{3}[-.\s]?\d{4}$/.test(val);
+      }, "Invalid phone number format"),
+    Practice_Address: z.string().min(1, "Practice Address is required"),
+    Lisence_Number: z.string().min(1, "License Number is required"),
+  })
+  .refine((data) => data.Password === data.Confirm_Password, {
+    message: "Passwords don't match",
+    path: ["Confirm_Password"],
+  });
 
 export const Reg = () => {
-    const[Resp, setResp] = useState("");
-    const[RegForm, setRegForm] = useState({
-            Username: "",
-            Password: "",
-            First_Name: "", 
-            Last_Name: "",
-            Date_of_Birth: "" ,
-            Practice_Email_Address: "",
-            Practice_Phone_Number: "",
-            Practic_Address: "",
-            Lisence_Number: "",
-            License_Expiration_Date: "",
-            Organization: "",
-            Specialization: ""
-    });
-    
-    async function submittion(e) {
-        e.preventDefault();
-        await axios.post("/Registration", {RegForm}).then(resp => console.log(resp))
-    }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: zodResolver(schema),
+  });
 
-    const inputs = (e) =>{
-        setRegForm({...RegForm, [e.target.name] : e.target.value})
+  const onSubmit = async (data) => {
+    try {
+      const response = await axios.post(
+        "http://141.215.219.104:8000/Registration",
+        data
+      );
+      alert("Registration successful");
+    } catch (error) {
+      console.error("Error submitting form", error);
+      alert("There was an issue with the registration");
     }
+  };
 
-    return (
-    <div>
-        <form onSubmit={submittion}>
-            Username: <input type ="text" onChange ={inputs} name ="Username"></input> <br></br>
-            Password: <input type ="text" onChange ={inputs} name ="Password"></input> <br></br>
-            First Name: <input type ="text" onChange ={inputs} name ="First_Name"></input> <br></br>
-            Last Name: <input type ="text" onChange ={inputs} name = "Last_Name"></input> <br></br>
-            Date of Birth: <input type ="date" onChange ={inputs} name = "Date_of_Birth"></input> <br></br>
-            Practice Email Address: <input  type ="text" onChange ={inputs} name = "Practice_Email_Address"></input> <br></br>
-            Practice Phone Number: <input type ="text" onChange ={inputs} name = "Practice_Phone_Number"></input> <br></br>
-            Practice Address: <input type ="text" onChange ={inputs} name = "Practic_Address"></input> <br></br>
-            Lisence Number: <input type ="number" onChange ={inputs} name = "Lisence_Number"></input> <br></br>
-            License Expiration Date: <input type ="date" onChange ={inputs} name = "License_Expiration_Date"></input> <br></br>
-            Organization: <input type ="text" onChange ={inputs} name = "Organization"></input> <br></br>
-            Specialization: <input type ="text" onChange ={inputs} name = "Specialization"></input> <br></br>
-            <button type="submit">Submit</button>
-        </form>
-    </div> 
-    );
+  return (
+    <div className="min-h-screen flex lg:flex-row md:flex-row flex-col">
+      {/* Left Section for logo and image */}
+      <div className="lg:md:w-1/2 w-[100%] bg-secondary flex flex-col justify-center items-center">
+        <header className="w-full py-4 px-8">
+          <img src="replenx-main-logo.png" alt="Logo" className="w-48" />
+        </header>
+        <img
+          src="doctor-image.png"
+          alt="Doctor Illustration"
+          className="h-auto p-12 w-[100%] object-cover"
+        />
+      </div>
+
+      {/* Right Section for the Form */}
+      <div className="lg:md:w-1/2 w-[100%] p-8 flex justify-center items-center">
+        <div className="form-container max-w-[500px]">
+          <h2 className="text-2xl font-bold text-gray-800 mb-4">
+            Account Sign Up
+          </h2>
+          <p className="text-gray-500 mb-6 pb-6 border-b border-b-gray-300">
+            Become a member and make managing your patients easier.
+          </p>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div>
+              <label htmlFor="Practice_Email_Address">
+                Practice Email Address
+              </label>
+              <input
+                type="text"
+                {...register("Practice_Email_Address")}
+                className={`w-full h-[45px] px-4 py-2 mt-2 mb-2 border rounded-lg focus:outline-none ${
+                  errors.Practice_Email_Address
+                    ? "border-red-500"
+                    : "border-gray-500"
+                } focus:ring-2 focus:ring-primary`}
+              />
+              {errors.Practice_Email_Address && (
+                <span className="text-red-500">
+                  {errors.Practice_Email_Address.message}
+                </span>
+              )}
+            </div>
+
+            <div>
+              <label htmlFor="Password">Password</label>
+              <input
+                type="password"
+                {...register("Password")}
+                className={`w-full h-[45px] px-4 py-2 mt-2 mb-2 border rounded-lg focus:outline-none ${
+                  errors.Password ? "border-red-500" : "border-gray-500"
+                } focus:ring-2 focus:ring-primary`}
+              />
+              {errors.Password && (
+                <span className="text-red-500">{errors.Password.message}</span>
+              )}
+            </div>
+
+            <div>
+              <label htmlFor="Confirm_Password">Confirm Password</label>
+              <input
+                type="password"
+                {...register("Confirm_Password")}
+                className={`w-full h-[45px] px-4 py-2 mt-2 mb-2 border rounded-lg focus:outline-none ${
+                  errors.Confirm_Password ? "border-red-500" : "border-gray-500"
+                } focus:ring-2 focus:ring-primary`}
+              />
+              {errors.Confirm_Password && (
+                <span className="text-red-500">
+                  {errors.Confirm_Password.message}
+                </span>
+              )}
+            </div>
+
+            <div>
+              <label htmlFor="First_Name">First Name</label>
+              <input
+                type="text"
+                {...register("First_Name")}
+                className={`w-full h-[45px] px-4 py-2 mt-2 mb-2 border rounded-lg focus:outline-none ${
+                  errors.First_Name ? "border-red-500" : "border-gray-500"
+                } focus:ring-2 focus:ring-primary`}
+              />
+              {errors.First_Name && (
+                <span className="text-red-500">
+                  {errors.First_Name.message}
+                </span>
+              )}
+            </div>
+
+            <div>
+              <label htmlFor="Last_Name">Last Name</label>
+              <input
+                type="text"
+                {...register("Last_Name")}
+                className={`w-full h-[45px] px-4 py-2 mt-2 mb-2 border rounded-lg focus:outline-none ${
+                  errors.Last_Name ? "border-red-500" : "border-gray-500"
+                } focus:ring-2 focus:ring-primary`}
+              />
+              {errors.Last_Name && (
+                <span className="text-red-500">{errors.Last_Name.message}</span>
+              )}
+            </div>
+
+            <div>
+              <label htmlFor="Date_of_Birth">Date of Birth</label>
+              <input
+                type="date"
+                {...register("Date_of_Birth")}
+                className={`w-full h-[45px] px-4 py-2 mt-2 mb-2 border rounded-lg focus:outline-none ${
+                  errors.Date_of_Birth ? "border-red-500" : "border-gray-500"
+                } focus:ring-2 focus:ring-primary`}
+              />
+              {errors.Date_of_Birth && (
+                <span className="text-red-500">
+                  {errors.Date_of_Birth.message}
+                </span>
+              )}
+            </div>
+
+            <div>
+              <label htmlFor="Practice_Phone_Number">
+                Practice Phone Number
+              </label>
+              <input
+                type="text"
+                {...register("Practice_Phone_Number")}
+                className={`w-full h-[45px] px-4 py-2 mt-2 mb-2 border rounded-lg focus:outline-none ${
+                  errors.Practice_Phone_Number
+                    ? "border-red-500"
+                    : "border-gray-500"
+                } focus:ring-2 focus:ring-primary`}
+              />
+              {errors.Practice_Phone_Number && (
+                <span className="text-red-500">
+                  {errors.Practice_Phone_Number.message}
+                </span>
+              )}
+            </div>
+
+            <div>
+              <label htmlFor="Practice_Address">Practice Address</label>
+              <input
+                type="text"
+                {...register("Practice_Address")}
+                className={`w-full h-[45px] px-4 py-2 mt-2 mb-2 border rounded-lg focus:outline-none ${
+                  errors.Practice_Address ? "border-red-500" : "border-gray-500"
+                } focus:ring-2 focus:ring-primary`}
+              />
+              {errors.Practice_Address && (
+                <span className="text-red-500">
+                  {errors.Practice_Address.message}
+                </span>
+              )}
+            </div>
+
+            <div>
+              <label htmlFor="Lisence_Number">License Number</label>
+              <input
+                type="number"
+                {...register("Lisence_Number")}
+                className={`w-full h-[45px] px-4 py-2 mt-2 mb-2 border rounded-lg focus:outline-none ${
+                  errors.Lisence_Number ? "border-red-500" : "border-gray-500"
+                } focus:ring-2 focus:ring-primary`}
+              />
+              {errors.Lisence_Number && (
+                <span className="text-red-500">
+                  {errors.Lisence_Number.message}
+                </span>
+              )}
+            </div>
+
+            <button
+              type="submit"
+              className="w-full h-[55px] bg-primary hover:bg-blue-600 text-white font-semibold py-2 rounded-lg transition-colors mt-4"
+            >
+              Submit
+            </button>
+          </form>
+
+          <p className="text-gray-600 p-6 mb-6 flex justify-center">
+            Already have an account?&nbsp;
+            <a href="/Login" className="text-primary hover:text-blue-700">
+              Login here
+            </a>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
