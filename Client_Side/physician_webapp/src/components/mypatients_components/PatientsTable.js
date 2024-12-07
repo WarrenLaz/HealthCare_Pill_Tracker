88edb1ee78ff29e6a0d6819238bfb31b123e44da
Heba Sayed <hebasayed1603@gmail.com>
2024-12-07 18:17:36 -0500

    slight style changes to dash and table

--- Client_Side/physician_webapp/src/components/mypatients_components/PatientsTable.js
+++ Client_Side/physician_webapp/src/components/mypatients_components/PatientsTable.js
@@ -5,21 +5,19 @@ import axios from "axios";
 import { useState, useEffect } from "react";
 import useAuth from "../../hooks/useAuth";
 
-
 const PatientsTable = () => {
-  const{auth} = useAuth();
+  const { auth } = useAuth();
   const [patient, setPatients] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
       try {
         const keys = auth.payload.p; // Extract keys from auth
         console.log(keys);
-        const res = await axios.get("http://localhost:8000/patients", 
-          {
-            headers: {
-              'Authorization': 'Bearer ' + String(auth.payload)
-            }
-          });
+        const res = await axios.get("http://localhost:8000/patients", {
+          headers: {
+            Authorization: "Bearer " + String(auth.payload),
+          },
+        });
         console.log(res.data);
         setPatients(res.data); // Update state with the fetched data
       } catch (error) {
@@ -28,9 +26,8 @@ const PatientsTable = () => {
     };
 
     fetchData();
-  }, [auth.payload.p]); 
+  }, [auth.payload.p]);
 
-  
   return (
     <div>
       <div className="flex justify-between px-16 pt-12">
@@ -47,9 +44,7 @@ const PatientsTable = () => {
         {/* Sort By Dropdown */}
         <div className="bg-slate-50 w-64 h-9 rounded-full flex items-center px-4">
           <select className="bg-transparent outline-none w-full text-slate-600 cursor-pointer">
-            <option value="">
-              Sort By
-            </option>
+            <option value="">Sort By</option>
             <option value="name">Alphabetical (A-Z)</option>
             <option value="option2">(another option here)</option>
           </select>
@@ -81,7 +76,10 @@ const PatientsTable = () => {
           <tbody>
             {/* Map over the dummy data */}
             {patient.map((patient, index) => (
-              <tr key={index} className="hover:bg-gray-100">
+              <tr
+                key={index}
+                className="hover:bg-gray-100  border-b  border-gray-300"
+              >
                 <td className="px-4 py-2">{patient.First_Name}</td>
                 <td className="px-4 py-2">{patient.Last_Name}</td>
                 <td className="px-4 py-2">{patient.Email_Address}</td>
--- Client_Side/physician_webapp/src/pages/Dashboard.js
+++ Client_Side/physician_webapp/src/pages/Dashboard.js
@@ -9,7 +9,6 @@ import PatientCount from "../components/mypatients_components/PatientCount";
 import PatientsTable from "../components/mypatients_components/PatientsTable";
 
 export const Dashboard = () => {
-
   const { auth } = useAuth();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [credentials, setCreds] = useState({});
@@ -22,13 +21,13 @@ export const Dashboard = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        console.log(auth.payload)
-        const res = await axios.get("http://localhost:8000/user",{
+        console.log(auth.payload);
+        const res = await axios.get("http://localhost:8000/user", {
           headers: {
-            'Authorization': 'Bearer ' + String(auth.payload)
-          }
+            Authorization: "Bearer " + String(auth.payload),
+          },
         });
-        console.log(res.data);// Update state with the fetched data
+        console.log(res.data); // Update state with the fetched data
         setCreds(res.data);
       } catch (error) {
         console.error("Error fetching doctors:", error);
@@ -36,17 +35,20 @@ export const Dashboard = () => {
     };
 
     fetchData();
-  }, [auth.payload]); 
+  }, [auth.payload]);
 
   console.log(auth.payload);
   return (
-    <div className="px-12 py-6 bg-secondary w-full h-full">
+    <div className="pr-12 pl-16 py-6 bg-secondary w-full h-full">
       <div className="flex-col items-center ">
-        <h1 className="text-4xl"> WELCOME DR.{String(credentials.Last_Name).toUpperCase()}</h1>
+        <h1 className="text-2xl font-semibold">
+          {" "}
+          Welcome Dr.{String(credentials.Last_Name)}
+        </h1>
       </div>
       <div className="flex mt-6 flex-col">
         <div className="flex justify-between">
-         <PatientCount />
+          <PatientCount />
           <button
             onClick={toggleModal}
             className="w-[100px] h-[100px] bg-white rounded-full flex justify-center items-center"
