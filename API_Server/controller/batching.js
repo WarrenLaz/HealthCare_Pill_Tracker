628a091794004749efa387b2f737aff981a792e9
Warren Lazarraga <79947453+WarrenLaz@users.noreply.github.com>
2025-03-26 20:54:47 -0400

    half way done

--- API_Server/controller/batching.js
+++ API_Server/controller/batching.js
@@ -2,10 +2,6 @@ const db = require("./dbcontroller");
 
 // const { ObjectId } = require("mongodb");
 // 
-
-var batches =      [20, 30, 60, 80, 10];
-var currentQuant = [[20, 0], [30, 0], [60, 0], [80, 0], [10,0]];
-const perDay =     [1,   2,  3,   4, 1];
 const Thresh_a = 12;
 const Thresh_b = 20;
 var reorder = false;
@@ -14,4 +10,4 @@ const Batching = (req, res) => {
 
 }
 
-module.exports = Log;
+module.exports = Batching;
