0113b9d0348c85a2db40e6797d05d217bb7abe1e
Heba Sayed <hebasayed1603@gmail.com>
2025-03-29 00:07:33 -0400

    green check mark on med card

--- Client_Side/patient_app/src/components/MiniMedCard.jsx
+++ Client_Side/patient_app/src/components/MiniMedCard.jsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { Pressable, Text, StyleSheet } from "react-native";
+import { Pressable, Text, StyleSheet, View } from "react-native";
 import Icon2 from "react-native-vector-icons/MaterialCommunityIcons";
 
-const MiniMedCard = ({ item, onPress }) => {
+const MiniMedCard = ({ item, onPress, taken }) => {
   return (
     <Pressable style={styles.medCard} onPress={() => onPress(item)}>
       <Text style={styles.medTitle}>
         <Icon2 name="pill" size={20} color="#3A3A3B" /> {item.MedName}
       </Text>
+
+      {/* Conditionally render the green circle with checkmark */}
+      {taken && (
+        <View style={styles.checkmarkContainer}>
+          <Icon2 name="check" size={20} color="white" />
+        </View>
+      )}
     </Pressable>
   );
 };
@@ -16,7 +23,9 @@ const styles = StyleSheet.create({
   medCard: {
     backgroundColor: "#fff",
     borderRadius: 10,
-    padding: 16,
+    paddingTop: 16,
+    paddingBottom: 16,
+    paddingRight: 16,
     marginBottom: 16,
     shadowColor: "#000",
     shadowOpacity: 0.1,
@@ -24,12 +33,21 @@ const styles = StyleSheet.create({
     elevation: 3,
     flexDirection: "row",
     alignItems: "center",
+    justifyContent: "space-between",
   },
   medTitle: {
-    fontSize: 20,
+    fontSize: 16,
     fontWeight: "bold",
     color: "#3A3A3B",
     marginLeft: 10,
+  },
+  checkmarkContainer: {
+    width: 24,
+    height: 24,
+    borderRadius: 12,
+    backgroundColor: "green",
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
 
